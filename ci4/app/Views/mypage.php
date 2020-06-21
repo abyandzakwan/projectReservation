656adc3036227721d417643b2d437c3f24f07f18
abyan.dzakwan <abyan.dzakwan@gmail.com>
2020-06-22 05:23:24 +0700

    Change home page

--- ci4/app/Views/mypage.php
+++ ci4/app/Views/mypage.php
@@ -1,5 +1,20 @@
 <html>
     <head>Welcome</head>
     <body>This is my PHP pages</body>
+    h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+?>
+
 
 </html>
